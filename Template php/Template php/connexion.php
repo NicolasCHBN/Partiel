f08028ed31f03fe8f6a9c9290d542137c40945a0
NicolasCHBN <115983884+NicolasCHBN@users.noreply.github.com>
2023-01-12 17:16:38 +0100

    Add files via upload

---
+++ Template php/Template php/connexion.php
@@ -0,0 +1,10 @@
+<?php
+    
+    /* page: inscription.php */
+//connexion à la base de données:
+<form action="action.php" method="post">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="number" name="age" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+<p></p>
